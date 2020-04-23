a86143a8572a7233bd0eb7551ae872c8499681b1
marcos Belice <marcos.belice@soulisto.com.br>
2020-04-23 16:20:19 -0300

    initial commit

---
+++ WebAdvert.Web/Models/Accounts/LoginModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAdvert.Web.Models.Accounts
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(6, ErrorMessage = "Password must be at least six characters long!")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
